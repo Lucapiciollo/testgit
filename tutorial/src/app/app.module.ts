6e32ac1e2f3182bc6ea822ec2b3476b37d0e53d8
lucarosati17 <lukarosati17@gmail.com>
2022-12-06 11:58:34 +0100

    06122022 12.00

--- tutorial/src/app/app.module.ts
+++ tutorial/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
- 
+
 import { SharedModule } from './shared/module/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-     
-  ],
+     ],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- tutorial/src/app/shared/module/shared.module.ts
+++ tutorial/src/app/shared/module/shared.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../component/menu/menu.component';
 import { RouterModule } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 
@@ -9,7 +11,8 @@ import { RouterModule } from '@angular/router';
   declarations: [MenuComponent],
   imports: [
     RouterModule,
-    CommonModule
+    CommonModule,
+    MatButtonModule
   ],
   exports:[MenuComponent]
 })
